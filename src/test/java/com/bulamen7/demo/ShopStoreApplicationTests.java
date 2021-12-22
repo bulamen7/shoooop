d27e2f0e0f3debd9e2eb8cb1a630632af3e5b928
Arkadiusz Swioklo <swiokloarkadiusz@gmail.com>
2021-12-22 16:43:00 +0100

    init project

---
+++ src/test/java/com/bulamen7/demo/ShopStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulamen7.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
