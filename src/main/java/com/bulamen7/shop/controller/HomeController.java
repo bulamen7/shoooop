a0b9408dd0ac309f1e3ae797359ff99a83716053
Arkadiusz Swioklo <swiokloarkadiusz@gmail.com>
2022-01-20 19:45:30 +0100

    add bootstrap

---
+++ src/main/java/com/bulamen7/shop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.bulamen7.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    ModelAndView homePage() {
+        return new ModelAndView("index");
+    }
+}
